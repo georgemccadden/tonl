632d36ef1ac7ba8b5fb4d58be7be63a73cd00a0d
@georgemccadden <georgemccadden@gmail.com>
2022-01-30 23:21:45 -0800

    Create React App

---
+++ comps/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>T O N L</h1>
+      <h2>Diverse Imagery</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+  );
+};
+
+export default Title;
